6ad19681d7a760bfd170103f956447d70ba0bb64
Tim Stadler <timstadler905@googlemail.com>
2015-07-31 21:13:50 +0200

    Comments and Cleanup

--- SenpaiCopy/SenpaiCopy/PathCheckBox.cs
+++ SenpaiCopy/SenpaiCopy/PathCheckBox.cs
@@ -2,10 +2,16 @@
 
 namespace SenpaiCopy
 {
+  /// <summary>
+  /// CheckBox that holds a folder path.
+  /// </summary>
   class PathCheckBox : CheckBox
   {
     private string _fullPath;
 
+    /// <summary>
+    /// Path to the folder.
+    /// </summary>
     public string FullPath
     {
       get { return _fullPath; }
